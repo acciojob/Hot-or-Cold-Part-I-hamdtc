5268dd8c7ce2eabaad8ba6ca3486f7d3e52c4dd0
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-02-18 06:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
 
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+
+btn.addEventListener("click", function() {
+  const randomNumber = Math.floor(Math.random() * 41) - 20;
+  num.textContent = randomNumber;
+});
+
+//get the input element
+const input = document.getElementById("guess");
+//get the respond element
+const respond = document.getElementById("respond");
+
+let correctNumber = Math.floor(Math.random() * 100);
+
+input.addEventListener("change", function() {
+  const guess = parseInt(input.value);
+  if (guess === correctNumber) {
+    respond.textContent = "Correct!";
+  } else if (Math.abs(guess - correctNumber) <= 10) {
+    respond.textContent = "Hot";
+  } else {
+    respond.textContent = "Cold";
+  }
+});
